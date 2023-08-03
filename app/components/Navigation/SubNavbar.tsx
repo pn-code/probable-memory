d1426d9dc4a619d36da7594d3deb437f9215e166
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-03 15:45:48 -0700

    Created SubNavbar

---
+++ app/components/Navigation/SubNavbar.tsx
@@ -0,0 +1,113 @@
+import React from "react";
+import Link from "next/link";
+import {
+    AppWindow,
+    CalendarHeart,
+    Compass,
+    Home,
+    Info,
+    Laugh,
+    Music,
+    ScrollText,
+    Users,
+    Video,
+} from "lucide-react";
+
+export default function SubNavbar() {
+    return (
+        <nav>
+            <ul className="flex w-full">
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <Home />
+                        <span>Home</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <Compass />
+                        <span>Browse</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <ScrollText />
+                        <span>Blog</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <AppWindow />
+                        <span>Forum</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <Users />
+                        <span>Groups</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <CalendarHeart />
+                        <span>Events</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <Video />
+                        <span>Videos</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <Music />
+                        <span>Music</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <Laugh />
+                        <span>Comedy</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        href="/"
+                    >
+                        <Info />
+                        <span>About</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
