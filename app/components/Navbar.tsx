7a240fb1cf3fa17db6f5f3327d30f13aed8f0650
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-03 15:23:48 -0700

    Updated Navbar with right side

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav className="bg-navyblue text-[#FFFF] px-4">
+        <nav className="w-full bg-navy-blue text-[#FFFF] px-4 py-2 flex justify-between">
             {/* Left Side & Brand Logo & Home Navigation */}
             <header className="flex gap-4 items-center">
                 <Users2 size={80} />
@@ -16,14 +16,55 @@ export default function Navbar() {
             </header>
 
             {/* Right Side */}
-            <section>
-                <ul>
-                    <li>
-                        <Globe2 /> International
+            <section className="flex flex-col gap-1 items-end">
+                {/* Upper Right Side */}
+                <ul className="flex gap-2.5 text-sm mt-1">
+                    <li className="flex gap-1 items-center">
+                        <Globe2 size={16} /> International
                     </li>
-                    <li>Help</li>
+                    <span>|</span>
+                    <li>Help </li>
+                    <span>|</span>
                     <li>Sign Up</li>
                 </ul>
+
+                {/* Bottom Right Side | Search Bar */}
+                <form className="flex flex-col sm:min-w-[500px]">
+                    <ul className="flex gap-2 text-sm text-gray-100">
+                        <li>
+                            <button>Web</button>
+                        </li>
+                        <span>|</span>
+                        <li>
+                            <button>People</button>
+                        </li>
+                        <span>|</span>
+                        <li>
+                            <button>Music</button>
+                        </li>
+                        <span>|</span>
+                        <li>
+                            <button>Blogs</button>
+                        </li>
+                        <span>|</span>
+                        <li>
+                            <button>Videos</button>
+                        </li>
+                        <span>|</span>
+                        <li>
+                            <button>Films</button>
+                        </li>
+                    </ul>
+                    <section className="flex justify-between gap-4">
+                        <input
+                            className="w-full rounded-sm text-black text-sm px-1"
+                            type="text"
+                        />
+                        <button className="bg-gray-100 text-black px-4 rounded-sm">
+                            Search
+                        </button>
+                    </section>
+                </form>
             </section>
         </nav>
     );
