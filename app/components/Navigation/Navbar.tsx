3714aa45977b36b774584107618d0609877f6352
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-04 15:41:49 -0700

    Added Link to li elements

--- app/components/Navigation/Navbar.tsx
+++ app/components/Navigation/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Globe2, Users2 } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
@@ -19,48 +20,54 @@ export default function Navbar() {
             <section className="flex flex-col gap-1 items-end">
                 {/* Upper Right Side */}
                 <ul className="flex gap-2.5 text-sm mt-1">
-                    <li className="flex gap-1 items-center">
-                        <Globe2 size={16} /> International
+                    <li>
+                        <Link className="flex gap-1 items-center hover:text-gray-300" href="/">
+                            <Globe2 size={16} /> International
+                        </Link>
                     </li>
                     <span>|</span>
-                    <li>Help </li>
+                    <li>
+                        <Link className=" hover:text-gray-300"  href="/">Help</Link>{" "}
+                    </li>
                     <span>|</span>
-                    <li>Sign Up</li>
+                    <li>
+                        <Link className=" hover:text-gray-300"  href="/">Sign Up</Link>
+                    </li>
                 </ul>
 
                 {/* Bottom Right Side | Search Bar */}
-                <form className="flex flex-col sm:min-w-[500px]">
+                <form className="flex flex-col sm:min-w-[500px] gap-1">
                     <ul className="flex gap-2 text-sm text-gray-100">
-                        <li>
+                        <li className="hover:text-gray-300">
                             <button>Web</button>
                         </li>
                         <span>|</span>
-                        <li>
+                        <li className="hover:text-gray-300">
                             <button>People</button>
                         </li>
                         <span>|</span>
-                        <li>
+                        <li className="hover:text-gray-300">
                             <button>Music</button>
                         </li>
                         <span>|</span>
-                        <li>
+                        <li className="hover:text-gray-300">
                             <button>Blogs</button>
                         </li>
                         <span>|</span>
-                        <li>
+                        <li className="hover:text-gray-300">
                             <button>Videos</button>
                         </li>
                         <span>|</span>
-                        <li>
+                        <li className="hover:text-gray-300">
                             <button>Films</button>
                         </li>
                     </ul>
-                    <section className="flex justify-between gap-4">
+                    <section className="flex justify-between gap-2">
                         <input
                             className="w-full rounded-sm text-black text-sm px-1"
                             type="text"
                         />
-                        <button className="bg-gray-100 text-black px-4 rounded-sm">
+                        <button className="bg-gray-100 text-black px-4 rounded-sm hover:bg-gray-300">
                             Search
                         </button>
                     </section>
