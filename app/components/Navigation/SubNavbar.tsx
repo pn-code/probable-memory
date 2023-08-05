b292661a9c813727994fa3a0d89c28b6050a0036
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-05 15:09:07 -0700

    Resized list items to probably fit in page

--- app/components/Navigation/SubNavbar.tsx
+++ app/components/Navigation/SubNavbar.tsx
@@ -16,101 +16,101 @@ import {
 
 export default function SubNavbar() {
     return (
-        <nav className="hidden sm:flex border-b border-navy-blue/50 mb-2">
+        <nav className="w-full hidden md:flex border-b border-navy-blue/50 mb-2">
             <ul className="flex w-full">
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <Home />
                         <span>Home</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <Compass />
                         <span>Browse</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <Flame/>
                         <span>Trending</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <ScrollText />
                         <span>Blog</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <AppWindow />
                         <span>Forum</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <Users />
                         <span>Groups</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <CalendarHeart />
                         <span>Events</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <Video />
                         <span>Videos</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <Music />
                         <span>Music</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <Laugh />
                         <span>Comedy</span>
                     </Link>
                 </li>
-                <li>
+                <li className="flex-1">
                     <Link
-                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4 w-[10vw]"
+                        className="flex flex-col text-gray-600 justify-center items-center text-sm hover:bg-blue-500 ease-linear duration-200 hover:text-gray-100 p-4"
                         href="/"
                     >
                         <Info />
