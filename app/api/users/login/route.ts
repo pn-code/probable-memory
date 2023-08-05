c4e79220ed2692316ee2f870a8ef7673bf9499d2
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-05 15:08:20 -0700

    Created User Login POST route

---
+++ app/api/users/login/route.ts
@@ -0,0 +1,56 @@
+import connectDB from "@/lib/connectDB";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connectDB();
+
+export async function POST(req: NextRequest) {
+    try {
+        const reqBody = await req.json();
+        const { email, password } = reqBody;
+
+        // check if user exists
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return NextResponse.json(
+                { error: "User does not exist" },
+                { status: 400 }
+            );
+        }
+
+        // check password against hashed password
+        const validPassword = await bcryptjs.compare(password, user.password);
+
+        if (!validPassword) {
+            return NextResponse.json(
+                { error: "Invalid password" },
+                { status: 400 }
+            );
+        }
+
+        // create token data
+        const tokenData = {
+            id: user._id,
+            email: user.email,
+        };
+
+        // create token
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY!, {
+            expiresIn: "1h",
+        });
+
+        const response = NextResponse.json({
+            message: "Login successful",
+            success: true,
+        });
+
+        response.cookies.set("token", token, { httpOnly: true });
+
+        return response;
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
