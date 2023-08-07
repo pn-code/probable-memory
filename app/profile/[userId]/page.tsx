c6b3f571a5529edde77d06f5bac0552cb52df924
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-07 16:42:17 -0700

    Added desktop styles

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -30,7 +30,6 @@ export default function ProfilePage({
 }) {
     const HOST_URL = process.env.NEXT_PUBLIC_HOST_URL;
 
-    console.log(HOST_URL);
     const [userData, setUserData] = useState<UserData>();
     const userFullName = `${userData?.firstName} ${userData?.lastName}`;
 
@@ -46,29 +45,33 @@ export default function ProfilePage({
     }, []);
 
     return (
-        <main className="flex flex-col px-2 gap-4 py-4">
+        <main className="xl:mx-[20%] flex flex-col px-2 gap-4 py-4 md:flex-row md:gap-10">
             {/* Left Side */}
-            <section className="flex flex-col gap-4">
+            <section className="md:w-[460px] flex flex-col gap-4">
                 {/* Profile Section */}
+                <div className="flex flex-col gap-4">
+                    {/* Name & Music Preferences */}
+                    <section className="px-2">
+                        <h2 className="font-bold text-xl">
+                            {userFullName}
+                        </h2>
+                        <span className="text-sm font-semibold">
+                            Hip Hop / Alt Rock
+                        </span>
+                    </section>
 
-                {/* Music Preferences */}
-                <section className="px-2">
-                    <h2 className="font-semibold text-lg">{userFullName}</h2>
-                    <span className="text-sm font-semibold">
-                        Hip Hop / Alt Rock
-                    </span>
-                </section>
+                    {/* User Picture, Status, and Location */}
+                    <section className="w-full flex gap-8 items-center">
+                        <User size={160} />
 
-                {/* User Picture, Status, and Location */}
-                <section className="flex gap-4 items-center">
-                    <User size={160} />
+                        <div className="block text-sm">
+                            <div className="">Here is my status</div>
+                            <p>California</p>
+                            <p>United States</p>
+                        </div>
+                    </section>
+                </div>
 
-                    <div className="block text-sm">
-                        <div className="">Here is my status</div>
-                        <p>California</p>
-                        <p>United States</p>
-                    </div>
-                </section>
                 {/* Music */}
 
                 {/* Contact Information */}
@@ -76,39 +79,39 @@ export default function ProfilePage({
                     <h3 className="px-2 bg-blue-400 text-white font-semibold">
                         Contact {userFullName}
                     </h3>
-                    <ul className="grid grid-cols-2 gap-1 bg-gray-50 p-2 text-blue-600">
+                    <ul className="grid grid-cols-2 gap-1 bg-gray-50 p-2 text-blue-900">
                         <li>
-                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-800 cursor-pointer">
+                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-500 cursor-pointer">
                                 <MessageCircle />
                                 <p>Send Message</p>
                             </div>
                         </li>
                         <li>
-                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-800 cursor-pointer">
+                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-500 cursor-pointer">
                                 <Mail />
                                 <p>Send Email</p>
                             </div>
                         </li>
                         <li>
-                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-800 cursor-pointer">
+                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-500 cursor-pointer">
                                 <UserPlus />
                                 <p>Add to Friends</p>
                             </div>
                         </li>
                         <li>
-                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-800 cursor-pointer">
+                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-500 cursor-pointer">
                                 <Users2 />
                                 <p>Add to Group</p>
                             </div>
                         </li>
                         <li>
-                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-800 cursor-pointer">
+                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-500 cursor-pointer">
                                 <Star />
                                 <p>Add to Favorites</p>
                             </div>
                         </li>
                         <li>
-                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-800 cursor-pointer">
+                            <div className="w-fit flex gap-2 items-center text-sm font-normal hover:text-blue-500 cursor-pointer">
                                 <AlertTriangle />
                                 <p>Block User</p>
                             </div>
@@ -120,61 +123,59 @@ export default function ProfilePage({
                 <section className="w-full px-2 py-1 border border-gray-800 rounded-sm">
                     <h3 className="font-semibold">DefinitelyNotMySpace URL</h3>
                     <Link
-                        className="text-sm text-navy-blue underline"
+                        className="text-sm text-blue-900 hover:text-blue-500 underline"
                         href={`/profile/${params.userId}`}
                     >{`${HOST_URL}/profile/${params.userId}`}</Link>
                 </section>
 
                 {/* General Info */}
-
-                <section className="border-2 border-blue-400 text-white font-semibold">
+                <section className="h-full w-full border-2 border-blue-400 text-white font-semibold">
                     <h3 className="bg-blue-400 px-2">
                         {userFullName}: General Info
                     </h3>
-                    <table className="w-full h-full">
-                        <tr>
-                            <th>
-                                <div className="w-[115px] text-left bg-blue-200 text-blue-600 m-1 py-1 px-2 text-sm">
+                    <div className="w-full p-1">
+                        <div className="flex w-full">
+                            <div>
+                                <div className="w-[110px] text-left bg-blue-200 text-blue-600 m-1 py-1 px-2 text-sm">
                                     Member Since
                                 </div>
-                            </th>
-                            <td className="w-full h-full">
-                                <div className=" text-left bg-blue-100 text-gray-900 m-1 py-1 px-2 text-sm">
+                            </div>
+                            <div>
+                                <div className=" text-left px-2 bg-blue-100 text-gray-900 m-1 py-1 text-sm w-full">
                                     3/1/2010
                                 </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                <div className="w-[115px] text-left bg-blue-200 text-blue-600 m-1 py-1 px-2 text-sm">
+                            </div>
+                        </div>
+                        <div className="flex w-full">
+                            <div>
+                                <div className="w-[110px] text-left bg-blue-200 text-blue-600 m-1 py-1 px-2 text-sm">
                                     Influences
                                 </div>
-                            </th>
-                            <td className="w-full h-full">
-                                <div className="text-left bg-blue-100 text-gray-900 m-1 py-1 px-2 text-sm">
+                            </div>
+                            <div>
+                                <div className="text-left px-2 bg-blue-100 text-gray-900 m-1 py-1 text-sm w-full">
                                     Eminem, Drake, Lil Wayne, Linkin Park
                                 </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                <div className="w-[115px] text-left bg-blue-200 text-blue-600 m-1 py-1 px-2 text-sm">
+                            </div>
+                        </div>
+                        <div className="flex w-full">
+                            <div>
+                                <div className="w-[110px] text-left bg-blue-200 text-blue-600 m-1 py-1 px-2 text-sm">
                                     Hobbies
                                 </div>
-                            </th>
-                            <td className="w-full h-full">
-                                <div className="text-left bg-blue-100 text-gray-900 m-1 py-1 px-2 text-sm">
+                            </div>
+                            <div>
+                                <div className="text-left px-2 bg-blue-100 text-gray-900 m-1 py-1 text-sm w-full">
                                     Dogging my butter
                                 </div>
-                            </td>
-                        </tr>
-                    </table>
+                            </div>
+                        </div>
+                    </div>
                 </section>
-                {/* In General Info: Table detailing member since, influences */}
             </section>
 
             {/* Right Side */}
-            <section>
+            <section className="md:w-[800px]">
                 <header>
                     <h3 className="bg-orange-300/60 text-orange-600 font-semibold px-2">
                         About {userFullName}
