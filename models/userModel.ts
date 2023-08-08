fdddd23e2e1fc0ea546caf99a11cc32aba68b6e5
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-07 17:39:50 -0700

    Updated user schema with timestamps and extra info

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,44 +1,53 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, "Please provide a first name"],
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, "Please provide a first name"],
+        },
+        lastName: {
+            type: String,
+            required: [true, "Please provide a last name"],
+        },
+        email: {
+            type: String,
+            required: [true, "Please provide an email"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Please provide an email"],
+        },
+        birthday: {
+            type: String,
+            required: [true, "Please provide a birthday"],
+        },
+        gender: {
+            type: String,
+            required: [true, "Please provide a gender"],
+        },
+        info: {
+            influences: { type: String },
+            hobbies: { type: String },
+            description: { type: String },
+            title: { type: String },
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        forgotPasswordToken: String,
+        forgotPasswordTokenExpiry: Date,
+        verifyToken: String,
+        verifyTokenExpiry: Date,
     },
-    lastName: {
-        type: String,
-        required: [true, "Please provide a last name"],
-    },
-    email: {
-        type: String,
-        required: [true, "Please provide an email"],
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, "Please provide an email"],
-    },
-    birthday: {
-        type: String,
-        required: [true, "Please provide a birthday"],
-    },
-    gender: {
-        type: String,
-        required: [true, "Please provide a gender"],
-    },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-    forgotPasswordToken: String,
-    forgotPasswordTokenExpiry: Date,
-    verifyToken: String,
-    verifyTokenExpiry: Date,
-});
+    { timestamps: { createdAt: "created_at" } }
+);
 
 const User = mongoose.models.users || mongoose.model("users", userSchema);
 
