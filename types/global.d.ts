d072f62800e2df470d855edc412934b39f4f16ba
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-07 17:39:29 -0700

    Added UserData interface to globals

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+interface UserData {
+    birthday: string;
+    email: string;
+    firstName: string;
+    gender: string;
+    isAdmin: boolean;
+    isVerified: boolean;
+    lastName: string;
+    _id: string;
+    created_at: string
+}
