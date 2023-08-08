d8db45b3b746e443ea6e947920e9e4bf8c611ccc
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-07 17:40:15 -0700

    Created formatDateFromISO helper

---
+++ helpers/formatDateFromISO.ts
@@ -0,0 +1,8 @@
+export default function formatDateFromISO(dateString: string) {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${month}/${day}/${year}`
+}
