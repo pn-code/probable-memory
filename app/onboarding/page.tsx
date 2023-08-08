2bdd08c83c869d70e9fe9fde70cfb6ccbea9893f
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-08 16:32:10 -0700

    Created Onboarding Page

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AccountForm from "../components/forms/AccountForm";
+
+export default function OnboardingPage() {
+    return (
+        <main className="w-full h-full">
+            <h1 className="text-2xl font-semibold bg-navy-blue text-white p-4 lg:rounded-t-sm">
+                Onboarding
+            </h1>
+            <p className="text-gray-800 bg-blue-200 text-sm font-semibold px-4 py-1">
+                Complete your profile now
+            </p>
+
+            <AccountForm />
+        </main>
+    );
+}
