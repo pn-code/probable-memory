846c38b14a3c903f3b3613985b0bc1b8af46f699
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-09 16:51:18 -0700

    Created onboarding post route

---
+++ app/api/users/onboarding/route.ts
@@ -0,0 +1,35 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import connectDB from "@/lib/connectDB";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function POST(req: NextRequest) {
+    try {
+        const reqBody = await req.json();
+        const { hobbies, location, bio, influences } = reqBody;
+
+        console.log(location)
+
+        const userId = getDataFromToken(req);
+
+        if (!userId) {
+            return NextResponse.json(
+                { error: "No user found" },
+                { status: 403 }
+            );
+        }
+
+        const user = await User.findByIdAndUpdate(userId, {
+            $set: { info: { hobbies, location, bio, influences } },
+        });
+
+        const savedUser = await user.save();
+
+        return NextResponse.json({ status: "complete", user: savedUser });
+    } catch (error: any) {
+        console.error(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
