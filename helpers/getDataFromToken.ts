c8d7d5885c0d39774557efc08e56ad8ed4401385
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-09 18:52:24 -0700

    Added case if token expires

--- helpers/getDataFromToken.ts
+++ helpers/getDataFromToken.ts
@@ -9,12 +9,22 @@ export const getDataFromToken = (req: NextRequest) => {
     try {
         const token = req.cookies.get("token")?.value || "";
 
-        const decodedToken = jwt.verify(
-            token,
-            process.env.JWT_SECRET_KEY!
-        ) as UserDecodedToken;
-
-        return decodedToken.id;
+        if (token) {
+            try {
+                const decodedToken = jwt.verify(
+                    token,
+                    process.env.JWT_SECRET_KEY!
+                ) as UserDecodedToken;
+    
+                return decodedToken.id;
+            } catch (error) {
+                console.error("Token is expired, clearing token...")
+                return null;
+            }
+        } else {
+            console.log("No token found");
+            return null;
+        }
     } catch (error: any) {
         throw new Error(error.message);
     }
