da2b4f9cfa93149228dbbffdf2af7d00a64787ca
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-11 13:49:40 -0700

    Refactored connectDB

--- lib/connectDB.ts
+++ lib/connectDB.ts
@@ -1,21 +1,16 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL as string;
-
-if (!MONGODB_URL) {
-    throw new Error(
-        "Please define the MONGODB_URI environment variable inside .env.local"
-    );
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-mongoose.set("strictQuery", true);
-
 export default async function connectDB() {
+    const MONGODB_URL = process.env.MONGODB_URL as string;
+
+    if (!MONGODB_URL) {
+        throw new Error(
+            "Please define the MONGODB_URI environment variable inside .env.local"
+        );
+    }
+
+    mongoose.set("strictQuery", true);
+
     try {
         await mongoose.connect(MONGODB_URL);
         console.log("Connected to MongoDB", MONGODB_URL);
