f34a26cc6e33509d7c7e5d857cf2ec7b5a420ce0
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-11 13:40:27 -0700

    Updated getCurrentUser

--- helpers/getCurrentUser.ts
+++ helpers/getCurrentUser.ts
@@ -1,11 +1,25 @@
-import axios from "axios";
+import connectDB from "@/lib/connectDB";
+import User from "@/models/userModel";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
 
 export async function getCurrentUser() {
     try {
-        const userData = await axios.get(
-            `${process.env.HOST_URL}/api/users/me`
-        );
-        return userData.data.user;
+        connectDB();
+        const cookieStore = cookies();
+        const token = cookieStore.get("token");
+
+        // verify with JWT
+        const user = jwt.verify(
+            token?.value!,
+            process.env.JWT_SECRET_KEY!
+        ) as any;
+
+        // isolate user id
+        const userId = user.id;
+        const currentUser = await User.findById(userId);
+
+        return currentUser;
     } catch (error) {
         console.error("No user found");
     }
